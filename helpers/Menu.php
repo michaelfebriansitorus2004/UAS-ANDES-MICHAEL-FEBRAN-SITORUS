7c357d088ffb65087d88df348eb42e3cf1743576
michaelfebriansitorus2004 <120706431+michaelfebriansitorus2004@users.noreply.github.com>
2022-12-16 13:57:50 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,233 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'personal_access_tokens', 
+			'label' => 'Personal Access Tokens', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_absensi_kerja', 
+			'label' => 'Tb Absensi Kerja', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_barang', 
+			'label' => 'Tb Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_barang_inventaris', 
+			'label' => 'Tb Barang Inventaris', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_detail_pembayaran', 
+			'label' => 'Tb Detail Pembayaran', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_detail_transaksi', 
+			'label' => 'Tb Detail Transaksi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_logging', 
+			'label' => 'Tb Logging', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_member', 
+			'label' => 'Tb Member', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_outlet', 
+			'label' => 'Tb Outlet', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_paket', 
+			'label' => 'Tb Paket', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_pembayaran', 
+			'label' => 'Tb Pembayaran', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_penjemputan', 
+			'label' => 'Tb Penjemputan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_transaksi', 
+			'label' => 'Tb Transaksi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_user', 
+			'label' => 'Tb User', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'personal_access_tokens', 
+			'label' => 'Personal Access Tokens', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_absensi_kerja', 
+			'label' => 'Tb Absensi Kerja', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_barang', 
+			'label' => 'Tb Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_barang_inventaris', 
+			'label' => 'Tb Barang Inventaris', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_detail_pembayaran', 
+			'label' => 'Tb Detail Pembayaran', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_detail_transaksi', 
+			'label' => 'Tb Detail Transaksi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_logging', 
+			'label' => 'Tb Logging', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_member', 
+			'label' => 'Tb Member', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_outlet', 
+			'label' => 'Tb Outlet', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_paket', 
+			'label' => 'Tb Paket', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_pembayaran', 
+			'label' => 'Tb Pembayaran', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_penjemputan', 
+			'label' => 'Tb Penjemputan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_transaksi', 
+			'label' => 'Tb Transaksi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'tb_user', 
+			'label' => 'Tb User', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
